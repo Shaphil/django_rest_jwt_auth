234bead74f3f42504619e9c250dc3e6e56ecac39
Shaphil <shaphil2025@gmail.com>
2018-06-20 15:15:01 +0600

    Add Custom JWT based authentication

---
+++ tutorial/quickstart/auth.py
@@ -0,0 +1,65 @@
+import jwt
+
+from django.contrib.auth.models import User
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from rest_framework.exceptions import AuthenticationFailed
+
+from tutorial.settings import SECRET_KEY
+
+ALGORITHM = 'HS256'
+
+# custom authentication: http://www.django-rest-framework.org/api-guide/authentication/#custom-authentication
+class JWTAuthentication(BaseAuthentication):
+    # override authenticate() method
+    def authenticate(self, request):
+        # get header info
+        header = get_authorization_header(request)
+        if header is None:
+            return None
+
+        # auth info will be two space separated values
+        # eg. bearer cnso9iduh0297wbeib
+        auth = header.split()
+        if len(auth) != 2:
+            raise AuthenticationFailed(
+                'Auth header must contain 2 values separated by space'
+            )
+
+        # the second part of the auth string is the token
+        token = auth[1]
+        return self.get_user(token), None
+
+    def get_user(self, token):
+      data = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+      user_id = data['id']
+      email = data['email']
+      user = User.objects.get(pk=user_id)
+      test_token = Utilities().encode_user(user)
+
+      if test_token != token:
+          raise AuthenticationFailed('Token mismatch, authentication failed')
+
+      return (user, token)
+
+
+class Utilities:
+    def encode(self, request):
+      user = self.get_user_from_request(request)
+      token = self.encode_user(user)
+
+      return token
+
+    def encode_user(self, user):
+      data = { 'id': user['id'], 'email': user['email'] }
+
+      return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM).decode('utf-8')
+
+    def get_user_from_request(self, request):
+      username = request.data['username']
+
+      try:
+        user = User.objects.get(username=username)
+      except User.DoesNotExits:
+        raise AuthenticationFailed('Invalid Username/Password')
+
+      return user
--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -3,13 +3,16 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 
 from quickstart.serializers import UserSerializer, GroupSerializer
+from quickstart.auth import JWTAuthentication
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    authentication_classes = [JWTAuthentication]
